2ada2cccb835c0cba3c3bd4505080a1622b46a67
ChrstnMgcn <paul@hispurposes.com>
2025-06-16 20:27:28 -0400

    Update update-company-defaults.js

--- netlify/functions/update-company-defaults.js
+++ netlify/functions/update-company-defaults.js
@@ -6,8 +6,8 @@
 // OLD: const authenticateAdmin = (authHeader) => { ... }; // This helper is removed
 
 // NEW: Import centralized utility functions (note the ../../ for path from admin/ subdirectory)
-const { createDbClient } = require('../../utils/db');
-const auth = require('../../utils/auth');
+const { createDbClient } = require('db');
+const auth = require('auth');
 
 exports.handler = async (event, context) => {
     // This function only accepts POST requests
