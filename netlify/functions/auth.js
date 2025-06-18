bc0c1dcb73990c193d363e4be9063738e08365a5
ChrstnMgcn <paul@hispurposes.com>
2025-06-17 20:39:47 -0400

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
  * @param {Object} event - The Netlify Function event object.
  * @returns {Object} An object containing statusCode and user data if successful, or an error response.
  */
-exports.handler = (event) => {
+exports.verifyToken = (event) => {
     const authHeader = event.headers.authorization;
     if (!authHeader) {
         return {
